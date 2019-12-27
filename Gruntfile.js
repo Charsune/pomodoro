dd89947944eefeac82355a0ac0fa170b3f2d7964
Charles <zaqqaz258z@gmail.com>
2019-12-27 22:19:24 +0800

    蕃茄鐘 程式碼優化 修改命名、調整切版邏輯、將css改為scss

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed',
+          quiet: true,
+          sourcemap: 'none',
+          cacheLocation: 'src/sass/'
+        },
+        files: {
+          'css/style.css': 'src/sass/style.scss'
+        }
+      }
+    },
+    watch: {
+      configFiles: {
+        files: ['Gruntfile.js'],
+        tasks: [],
+        options: {
+          livereload: 33334
+        }
+      },
+      css: {
+        files: ['**/*.scss'],
+        tasks: ['sass'],
+        options: {
+          livereload: 33334,
+        },
+      },
+      html: {
+        files: ['*.html'],
+        tasks: [],
+        options: {
+          livereload: 33334,
+        },
+      },
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass'); 
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['sass']);
+}
